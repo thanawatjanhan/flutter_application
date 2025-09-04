766486a081aee9edfab7107c85283eb7edd6e9c9
thanawatjanhan <6631501046@lamduan.mfu.ac.th>
2025-09-04 14:53:30 +0700

    Add basic app codes

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/week01/basic.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text('My First App')),
-        body: Text('Hello, World!'),
-        floatingActionButton: FloatingActionButton(
-          onPressed: null,
-          child: Text("Click"),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Basic())); //MaterialApp
 }
--- lib/week01/basic.dart
+++ lib/week01/basic.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+class Basic extends StatelessWidget {
+  const Basic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('My First App')),
+      body: Text('Hello, World!'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: null,
+        child: Text("Click"),
+      ),
+    );
+  }
+}
