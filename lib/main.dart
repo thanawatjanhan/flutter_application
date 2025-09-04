522acabcd66a6bef1f917f1d93c50caae787e19c
thanawatjanhan <6631501046@lamduan.mfu.ac.th>
2025-09-04 16:54:22 +0700

    Update TextField and link it to TextEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -4,5 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_1/week04/input_demo.dart';
 
 void main() {
-  runApp(MaterialApp(home: InputDemo())); //MaterialApp
+  runApp(
+    MaterialApp(home: InputDemo(), debugShowCheckedModeBanner: false),
+  ); //MaterialApp
 }
--- lib/week04/input_demo.dart
+++ lib/week04/input_demo.dart
@@ -23,8 +23,27 @@ class _InputDemoState extends State<InputDemo> {
       appBar: AppBar(title: Text("Input Demo")),
       body: Column(
         children: [
-          TextField(controller: tcName),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: tcName,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Input your name",
+                suffixIcon: IconButton(
+                  onPressed:
+                      () // => tcName.clear()
+                      {
+                        tcName.clear();
+                      },
+                  icon: Icon(Icons.clear),
+                ),
+              ),
+            ),
+          ),
+          // SizedBox(height: 16),
           ElevatedButton(onPressed: updateMessage, child: Text("Okay")),
+          SizedBox(height: 16),
           Text(message),
         ],
       ),
