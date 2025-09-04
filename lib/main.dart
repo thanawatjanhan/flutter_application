08f945b3fe5efc6d3942ecdec414baf8c7cfdf84
thanawatjanhan <6631501046@lamduan.mfu.ac.th>
2025-09-04 16:05:49 +0700

    Rename folder week01 to week04

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 // import 'package:flutter_application_1/week01/basic.dart';
-import 'package:flutter_application_1/week01/counter.dart';
+import 'package:flutter_application_1/week04/counter.dart';
 
 void main() {
-  runApp(MaterialApp(home: 
-    Counter()
-  )); //MaterialApp
+  runApp(MaterialApp(home: Counter())); //MaterialApp
 }
