0662314da0f62e926d79a27438f83e3333ca78e7
thanawatjanhan <6631501046@lamduan.mfu.ac.th>
2025-09-25 15:44:59 +0700

    Uncomplete Fishing Game

--- lib/fishing6631501046.dart
+++ lib/fishing6631501046.dart
@@ -31,6 +31,86 @@ class _FishingState extends State<Fishing> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Fishing Game',
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Spacer(),
+          Expanded(
+            flex: 3,
+            child: Column(
+              children: [
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Worms:"),
+                    Icon(Icons.waves_rounded, color: Colors.red),
+                    Icon(Icons.waves_rounded, color: Colors.red),
+                    Icon(Icons.waves_rounded, color: Colors.red),
+                    Icon(Icons.waves_rounded, color: Colors.red),
+                    Icon(Icons.waves_rounded, color: Colors.red),
+                  ],
+                ),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [Icon(Icons.currency_exchange)],
+                ),
+                Text("5"),
+              ],
+            ),
+          ),
+          // const SizedBox(height: 5),
+          Expanded(
+            flex: 4,
+            child: Image.asset(
+              'assets/images/fish/fisherman.png',
+              width: 200,
+              height: 200,
+              fit: BoxFit.contain,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Expanded(
+            flex: 3,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.green,
+                  ),
+                  child: const Text(
+                    "Start",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 5),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  child: const Text(
+                    "Reset",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+                const Spacer(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
